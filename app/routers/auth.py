feb9f2bab9115c3dd8be746c798907306ed0312c
mhmdrz22 <mhmdrdaqnbry2025@gmail.com>
2025-07-29 06:05:13 +0330

    Update auth.py

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -115,32 +115,8 @@ def login_for_access_token(
     return {
         "access_token": access_token,
         "token_type": "bearer",
+        "user": schemas.User.model_validate(user) 
     }
        
     }
 
-
-@router.post("/token")
-def login_for_access_token(
-    db: Session = Depends(get_db), form_data: OAuth2PasswordRequestForm = Depends()
-):
-    """
-    OAuth2 compatible token login, get an access token for future requests.
-    """
-    user = user_service.authenticate(
-        db, email=form_data.username, password=form_data.password
-    )
-    if not user:
-        raise HTTPException(status_code=400, detail="Incorrect email or password")
-
-    access_token = security.create_access_token(
-        subject=user.email,
-        user_id=user.id,
-        user_role=user.role.value,
-        full_name=user.full_name,
-        is_superuser=user.is_superuser,
-    )
-    return {
-        "access_token": access_token,
-        "token_type": "bearer",
-    }
